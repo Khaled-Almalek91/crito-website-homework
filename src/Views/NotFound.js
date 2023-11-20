1840428c605f1f3307b307ead3767c2397980209
Khaled-Almalek91 <khaled_king1991@hotmail.com>
2023-11-20 09:36:30 +0100

    validation done

--- src/Views/NotFound.js
+++ src/Views/NotFound.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import '../NotFound.scss';
+
 
 const NotFound = () => {
   return (
-    <h1>404 page Not Found</h1>
+    <>
+    
+    <div className="wrapper">
+        
+        <div className='notfoundsection'>
+        <h1> 404 page Not Found</h1>
+        </div>    
+        
+        <Footer/>
+ 
+    </div>
+    </>
   )
 }
 
--- src/components/MessageFormola.js
+++ src/components/MessageFormola.js
@@ -1,23 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
+
+
 
 const MessageFormola = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = {};
+
+    if (!formData.name.trim()) {
+      validationErrors.name = "Name is required";
+    }
+
+    if (!formData.email.trim()) {
+      validationErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      validationErrors.email = "Email is not valid";
+    }
+
+    if (!formData.message.trim()) {
+      validationErrors.message = "Message is required";
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      alert("Form submitted successfully");
+    }
+  }
+
+
+
+
+
   return (
     <div>
-        <div className="message">
-    <div className="container">
-      <h2>Leave us a message</h2>
-      <h2>for any information.</h2>
-      <div className="input-info">
-        <input type="text" placeholder="Name*" />
-        <input type="text" placeholder="Email*" />
-        <input type="text" id="M" placeholder="Your Message*" />
+      <div className="message-form">
+      <div className="container">
+
+
+        <form onSubmit={handleSubmit} method="post"  >
+
+          <h2>Leave us a message</h2>
+           <h2>for any information.</h2>
+             <div className="input-info">
+              <>
+              <input id="name" type="text" placeholder="Name*" onChange={handleChange} />
+               {errors.name && <span id="name-error">{errors.name} </span>}
+
+                 </>
+        
+                 <>
+                 <input id="email" type="text" placeholder="Email*" onChange={handleChange} />
+                {errors.email  && <span id="email-error">{errors.email} </span>}
+
+                 </>
+        
+                 <>
+                 <input id="message" type="text" placeholder="Your Message*" onChange={handleChange} />
+                 {errors.message && <span id="message-error">{errors.message} </span>}
+
+                    </>
+        
       </div>
       <div className="button-sendMessage">
-        <a className="btn-yellow" href="services.html">
-          Browse Team
-          <i className="fa-solid fa-arrow-up-right-from-square" />{" "}
-        </a>
+        <button type="submit" className="btn-yellow"> Submit <i className="fa-solid fa-arrow-up-right-from-square" />
+        </button>
       </div>
+
+      </form>
     </div>
   </div>
   </div>
